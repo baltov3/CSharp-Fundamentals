db6dd1c99bd3be0e010f4e5c7d442e60f5039025
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-05-30 13:23:11 +0300

    Create Progrm.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/10. Multiplication Table/Progrm.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{a} X {i} = {a*i}");
+             }
+           
+        }
+    }
+}
