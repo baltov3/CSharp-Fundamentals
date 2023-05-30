da495cf6973300495e72527339e466ffe031d0eb
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-05-30 13:16:45 +0300

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/06. Foreign Languages/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            if (country=="USA"||country== "England")
+            {
+                Console.WriteLine("English");
+            }
+            else if (country == "Spain" || country == "Mexico" || country == "Argentina")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
