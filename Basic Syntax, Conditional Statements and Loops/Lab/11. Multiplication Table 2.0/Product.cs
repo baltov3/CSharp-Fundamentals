01566ed380e0229cf770dd9fac0f4f7b3a09e947
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-05-31 17:22:01 +0300

    Create Product.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/11. Multiplication Table 2.0/Product.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+           
+            for (int i = b; i <= 10; i++)
+            {
+                Console.WriteLine($"{a} X {i} = {a*i}");
+             }
+            if (b>10)
+            {
+                Console.WriteLine($"{a} X {b} = {a * b}");
+            }
+           
+        }
+    }
+}
