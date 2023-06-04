5eb926de4e71e3b517900960a65f86ce93c75604
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-06-04 18:41:00 +0300

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/10. Rage Expenses /Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double rageExpenses = (lostGames / 2) * headsetPrice + (lostGames / 3) * mousePrice + (lostGames / 6) * keyboardPrice + (lostGames / 12) * displayPrice;
+
+            
+
+            Console.WriteLine($"Rage expenses: {rageExpenses:f2} lv.");
+        }
+    }
+}
